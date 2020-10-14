e096647f54e769877a488389d4e2048eb1942d12
e-farias <farias.softwares@gmail.com>
2020-10-14 15:46:29 -0300

    Refactoring the navbar

--- Web/js/scripts.js
+++ Web/js/scripts.js
@@ -1,7 +1,20 @@
-const hamburger = document.querySelector(".hamburger");
+// ---- MENU BTN HAMBURGER ---- //
 const menuLinks = document.querySelector(".menu-links");
 const links = document.querySelectorAll(".menu-links li");
+const btnMenuLinks = document.querySelector(".btn-menu-links");
+let btnMenuLinksOpen = false;
 
-hamburger.addEventListener("click", () => {
-    menuLinks.classList.toggle("open");
+btnMenuLinks.addEventListener("click", () => {
+    
+    if(!btnMenuLinksOpen) {
+        menuLinks.classList.toggle("open");
+        btnMenuLinks.classList.add("open");
+        btnMenuLinksOpen = true;
+    }
+    else {
+        menuLinks.classList.toggle("open");
+        btnMenuLinks.classList.remove("open");
+        btnMenuLinksOpen = false;
+    }
+
 });
