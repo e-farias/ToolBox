45888985b2ab58e308cb3461098c8a5705156723
e-farias <farias.softwares@gmail.com>
2020-10-15 13:17:08 -0300

    Add auto close navbar when click on link

--- Web/js/scripts.js
+++ Web/js/scripts.js
@@ -1,20 +1,32 @@
 // ---- MENU BTN HAMBURGER ---- //
 const menuLinks = document.querySelector(".menu-links");
-const links = document.querySelectorAll(".menu-links li");
 const btnMenuLinks = document.querySelector(".btn-menu-links");
 let btnMenuLinksOpen = false;
 
 btnMenuLinks.addEventListener("click", () => {
     
-    if(!btnMenuLinksOpen) {
-        menuLinks.classList.toggle("open");
-        btnMenuLinks.classList.add("open");
-        btnMenuLinksOpen = true;
-    }
-    else {
+    if(btnMenuLinksOpen) {
         menuLinks.classList.toggle("open");
         btnMenuLinks.classList.remove("open");
         btnMenuLinksOpen = false;
     }
+    else {
+        menuLinks.classList.toggle("open");
+        btnMenuLinks.classList.add("open");
+        btnMenuLinksOpen = true;
+    }
 
+});
+
+$('.menu-links li').click(() => {
+
+    if(btnMenuLinksOpen) {
+        menuLinks.classList.toggle("open");
+        btnMenuLinks.classList.remove("open");
+        btnMenuLinksOpen = false;
+    }
+    else {
+        pass;
+    }
+    
 });
